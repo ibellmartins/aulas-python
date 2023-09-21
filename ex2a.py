b579ef7e921c8edb512086596900dd3924a6184b
Isabella Sofia Martins <106943011+ibellmartins@users.noreply.github.com>
2023-09-21 11:01:32 -0300

    Add files via upload

---
+++ ex2a.py
@@ -0,0 +1,28 @@
+
+vermelho = '\033[31m'
+verde = '\033[32m'
+amarelo = '\033[33m'
+azul = '\033[34m'
+branco = '\033[37m'
+
+opcao = 0
+while opcao != 5:
+     print("Mack cores")
+     print("1-vermelho")
+     print("2-azul")
+     print("3-verde")
+     print("4-amarelo")
+     print("5-branco")
+     print("6-sair")
+     opcao = int(input("informe sua opção: "))
+
+     if opcao == 1:
+         print(vermelho, "MACKENZIE")
+     elif opcao == 2:
+         print(azul, "MACKENZIE")
+     elif opcao == 3:
+         print(verde, "MACKENZIE")
+     elif opcao == 4:
+         print(amarelo, "MACKENZIE")
+     elif opcao == 5:
+         print(branco, "MACKENZIE")

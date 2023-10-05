d0b716a28c8c2f705afcbc2d82506469c9220257
Isabella Sofia Martins <106943011+ibellmartins@users.noreply.github.com>
2023-10-05 10:36:19 -0300

    Add files via upload
    Exercicio dia 05/10

---
+++ exerciciodia5.py
@@ -0,0 +1,27 @@
+maximo = 5
+cont = 0
+avaliaOtimo = 0
+avaliaPessimo = 0
+idadeTotal = 0
+maioridadeBom = 0
+for num in range(1, maximo + 1):
+    print(f"Pessoa {num}: ")
+    idade = int(input("digite sua idade: "))  #guarda a idade, porém não avalia.
+    idadeTotal += idade
+    cont += 1
+    if idade >= 14:
+        print("A - Otimo\n B - Bom\n C - Regular\n D - Ruim\n E - Péssimo ")
+        avaliacao = input("digite sua avalição: ").upper()
+        if avaliacao == "A":
+            avaliaOtimo += 1
+        elif avaliacao == "B" and idade > maioridadeBom:
+            maioridadeBom = idade
+        elif avaliacao == "D":
+            avaliaPessimo += 1
+
+mediaIdade = idadeTotal / cont
+percentPessimo = (avaliaPessimo / cont) * 100
+print(f"A quantidade de respostas Ótimo foi {avaliaOtimo}")
+print(f"A média da idades é {mediaIdade}")
+print(f"A porcentagem de respostas Péssimo é {percentPessimo}")
+print(f"A maior idade entre as pessoas que responderam Bom é {maioridadeBom}")
